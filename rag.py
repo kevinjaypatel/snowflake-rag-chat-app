fd753fd95af0363553c092e40fcc879dac21af64
kevinjaypatel <kevinjaypatel@gmail.com>
2025-01-19 15:03:51 -0800

    added num chunks as a default parameter for creating a RAG instance

--- rag.py
+++ rag.py
@@ -1,13 +1,13 @@
 from trulens.apps.custom import instrument
 from search import CortexSearchRetriever
-from snowflake_setup import get_snowpark_session
+
 from snowflake.snowpark.session import Session
 from snowflake.cortex import Complete
 
 class RAG_from_scratch:
 
-    def __init__(self, snowpark_session: Session):
-        self.retriever = CortexSearchRetriever(snowpark_session=snowpark_session, limit_to_retrieve=4)
+    def __init__(self, snowpark_session: Session, num_chunks: int = 4):
+        self.retriever = CortexSearchRetriever(snowpark_session=snowpark_session, limit_to_retrieve=num_chunks)
 
     # @instrument
     def retrieve_context(self, query: str, filter_obj: dict = None) -> dict:
@@ -40,13 +40,15 @@ class RAG_from_scratch:
         """
         return Complete(model_name, prompt)
 
-    @instrument
+    
+    # @instrument
     def query(self, query: str) -> str:
         context_str = self.retrieve_context(query)
-        return self.generate_completion(query, context_str)
+        return self.generate_completion_with_context(query, context_str)
 
 if __name__ == "__main__":
     try: 
+        from snowflake_setup import get_snowpark_session
         session = get_snowpark_session()
         rag = RAG_from_scratch(session)
         query = "What is the rust programming language?"
