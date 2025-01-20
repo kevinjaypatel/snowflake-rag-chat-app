993f2cfa2a9010c6fa6bff7b09444486a72d6316
kevinjaypatel <kevinjaypatel@gmail.com>
2025-01-19 21:59:08 -0800

    updated the prompt for generating the response

--- rag.py
+++ rag.py
@@ -24,9 +24,9 @@ class RAG_from_scratch:
         """
         
         prompt = f"""
-          You are an expert assistant extracting information from context provided.
-          Answer the question based on the context. Be concise and do not hallucinate.
-          If you don´t have the information just say so.
+          You are helping a developer learn the Rust programming language. Exract the information from context provided, 
+          and answer the question based on the context. Be concise and do not hallucinate. 
+          If you don´t have the information just say so. Please provide examples and code snippets when possible.
           Context: {context_str}
           Question:
           {query}
